2a08cd8ac35d98fc43277aadaf9b2c748bc5a065
adarsha <022BSCIT005@sxc.edu.np>
2025-01-04 09:48:12 +0545

    get by placeholder text

--- src/components/application/Application.test.tsx
+++ src/components/application/Application.test.tsx
@@ -24,6 +24,9 @@ describe("Application", () => {
       selector: "input",
     });
     expect(nameElement2).toBeInTheDocument();
+
+    const nameElement3 = screen.getByPlaceholderText("Fullname");
+    expect(nameElement3).toBeInTheDocument();
     const bioElement = screen.getByRole("textbox", { name: "Bio" });
     expect(bioElement).toBeInTheDocument();
 
